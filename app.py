fe84df6de954bbd506516c5d5b8819facd871048
MarcinDudek <dudek.v@gmail.com>
2024-03-19 09:09:27 +0100

    add check if Pytho 3.12 or later

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import atexit
 import logging.config
 import yaml
 import logging
+import sys
 
 
 # Load the config file
@@ -14,11 +15,14 @@ logging.config.dictConfig(config)
 # Get a logger object
 logger = logging.getLogger(__name__)
 
-# Start the queue listener
-queue_handler = logging.getHandlerByName('queue_handler')
-if queue_handler is not None:
-    queue_handler.listener.start()
-    atexit.register(queue_handler.listener.stop)
+# Start the queue listener if Pyhon 3.12 or later
+if sys.version_info >= (3, 12):
+    queue_handler = logging.getHandlerByName('queue_handler')
+    if queue_handler is not None:
+        queue_handler.listener.start()
+        atexit.register(queue_handler.listener.stop)
+else:
+    logger.warning('.getHandlerByName is not available in this version of Python')
 
 # Log some messages
 logger.debug('This is a debug message')
