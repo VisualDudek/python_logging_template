40ac7fa77880f79800ae524f615e27b6cce62892
MarcinDudek <dudek.v@gmail.com>
2024-03-20 12:59:58 +0100

    add warn and debug msg

--- helper.py
+++ helper.py
@@ -3,4 +3,5 @@ import logging
 logger = logging.getLogger(__name__)
 
 def show_logging_form_diff_module():
+    logger.warning('This is a warning message form helper module')
     logger.debug('This is a debug message form helper module')
